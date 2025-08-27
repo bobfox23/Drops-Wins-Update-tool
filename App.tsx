8e03e11707c7c67f4407e6216ea4a94a64e72f04
bobfox23 <bob.fox@digibeat.com>
2025-08-27 17:20:39 +0200

    babel fix

--- App.tsx
+++ App.tsx
@@ -1,8 +1,7 @@
-
 import React, { useState } from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import TextAreaInput from './components/TextAreaInput';
+import Header from './components/Header.tsx';
+import Footer from './components/Footer.tsx';
+import TextAreaInput from './components/TextAreaInput.tsx';
 
 const App: React.FC = () => {
     const [mainData, setMainData] = useState<string>('');
